7bc2129e68d50c13de48cd347ec24ee8df53c88a
ottingerg <georg.ottinger@otelo.or.at>
2014-10-11 18:44:43 +0200

    added read_csi1_cfg_reg / NOT WORKING !!!

---
+++ kernel_modules/read_csi1_cfg_reg.c
@@ -0,0 +1,60 @@
+#include <linux/kernel.h> /* Needed for Macros*/
+#include <linux/module.h> /* Needed for all kernel modules*/
+#include <linux/init.h> 
+#include <linux/version.h>
+#include <linux/vmalloc.h>
+#include <linux/delay.h>
+#include <asm/io.h>
+
+#define CCM_BASE_ADDR  0x01C20000
+#define CSI1_BASE_ADDR 0x01C1D000
+#define TWI_STAT_REG 0x01C2B000
+
+#define CSI1_CFG_REG 0x004
+#define TWI_STAT_OFF 0x010
+#define CCM_CSI1_CLK 0x138
+
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("This is a my First Test Module...!");
+MODULE_AUTHOR("GVK51");
+
+
+static int __init my_start_init(void){
+		int reg;
+		void __iomem *io;
+
+        io = ioremap(CCM_BASE_ADDR, 256);
+		iowrite32((1 << 31), io + CCM_CSI1_CLK);
+		reg = ioread32(io + CCM_CSI1_CLK);
+		printk(KERN_INFO "ccm_csi1_clk: 0x%08x!\n",reg);
+        iounmap(io);
+
+		mdelay(500);
+
+		io = ioremap(CSI1_BASE_ADDR, 256);
+
+		if(io == NULL)
+			printk(KERN_INFO "ioremap error!\n");
+		else
+			printk(KERN_INFO "ioremap ok!\n");
+
+		iowrite32(0x1, io);
+
+		mdelay(500);
+
+        reg = ioread32(io);
+
+        printk(KERN_INFO "Register = 0x%08x\n", reg);
+
+		iounmap(io);
+
+        return 0;
+}
+
+static void __exit my_remove_exit(void){
+        printk(KERN_INFO "Module Un-loaded...!\n");  
+
+}
+
+module_init(my_start_init);
+module_exit(my_remove_exit);
